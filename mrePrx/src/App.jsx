c1c94784da98fb3eae6678e556dbaa73476dc42c
Arnav Sharma <136270908+arnnvv@users.noreply.github.com>
2024-01-15 07:57:02 +0530

    contextAPI

--- mrePrx/src/App.jsx
+++ mrePrx/src/App.jsx
@@ -1,52 +1,40 @@
 import { useState } from "react";
-import { CountContext } from "./context";
+import { CountContext } from "./context.js";
+import "./App.css";
 import { useContext } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
+    <>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count />
       </CountContext.Provider>
-    </div>
-  );
-}
-
-function Count({ setCount }) {
-  return (
-    <div>
-      <CountRenderer />
-      <Buttons setCount={setCount} />
-    </div>
+    </>
   );
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  const { count } = useContext(CountContext);
   return <div>{count}</div>;
 }
 
-function Buttons({ setCount }) {
-  const count = useContext(CountContext);
+function Buttons() {
+  const { count, setCount } = useContext(CountContext);
   return (
     <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
+      <button onClick={() => setCount(count + 1)}>Inc</button>
+      <button onClick={() => setCount(count - 1)}>Dec</button>
+    </div>
+  );
+}
 
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
     </div>
   );
 }
