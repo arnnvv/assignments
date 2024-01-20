1c4793c573cff2fe00179aa42fe70c1c0e0e6b03
arnnvv <arnav.900@protonmail.com>
2024-01-21 01:31:13 +0530

    wrprFx

--- wrapper/src/Wrapper.tsx
+++ wrapper/src/Wrapper.tsx
@@ -12,8 +12,32 @@ export function Wrapper() {
         onChange={(e) => setUsername(e.currentTarget.value)}
       />
       <div>
-        {JSON.stringify(
-          user.state === "loading" ? "loading..." : user.contents,
+        {user.state === "loading" ? (
+          <p>Loading...</p>
+        ) : user.state === "hasValue" && user.contents ? (
+          <>
+            <img src={user.contents.avatar_url} alt="User Avatar" />
+            <div className="user-details">
+              <h2>{user.contents.name ?? "No Name"}</h2>
+              <p>{user.contents.bio ?? "No Bio"}</p>
+              <p>
+                <strong>Location:</strong>{" "}
+                {user.contents.location ?? "No Location"}
+              </p>
+              <p>
+                <strong>Public Repositories:</strong>{" "}
+                {user.contents.public_repos ?? "0"}
+              </p>
+              <p>
+                <strong>Followers:</strong> {user.contents.followers ?? "0"}
+              </p>
+              <p>
+                <strong>Following:</strong> {user.contents.following ?? "0"}
+              </p>
+            </div>
+          </>
+        ) : (
+          <p>Error loading user data</p>
         )}
       </div>
     </div>
