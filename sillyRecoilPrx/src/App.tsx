8a5ca151cfcb8946039771a29dcb9ff8cf589a18
arnnvv <arnav.900@protonmail.com>
2024-01-20 14:14:24 +0530

    errHandling

--- sillyRecoilPrx/src/App.tsx
+++ sillyRecoilPrx/src/App.tsx
@@ -15,47 +15,60 @@ function App() {
 }
 
 function MainApp() {
-  const notifications = useRecoilValueLoadable(notificationsAtom);
-  const total = useRecoilValueLoadable(totalSelector);
+  try {
+    const notifications = useRecoilValueLoadable(notificationsAtom);
+    const total = useRecoilValueLoadable(totalSelector);
 
-  const renderValue = (value: number) => {
-    return value >= 100 ? "99+" : value;
-  };
+    const renderValue = (value: number) => {
+      return value >= 100 ? "99+" : value;
+    };
 
-  const renderButton = (label: string, value: number) => {
+    const renderButton = (label: string, value: number) => {
+      return (
+        <button>
+          {label}(
+          {notifications.state === "loading"
+            ? "loading..."
+            : renderValue(value)}
+          )
+        </button>
+      );
+    };
+
     return (
-      <button>
-        {label}(
-        {notifications.state === "loading" ? "loading..." : renderValue(value)})
-      </button>
+      <>
+        <button>HOME</button>
+        {renderButton("NETWORK", notifications.contents.network)}
+        {renderButton("MESSAGES", notifications.contents.messaging)}
+        {renderButton("JOBS", notifications.contents.jobs)}
+        {renderButton("NOTIFICATIONS", notifications.contents.notifications)}
+        <button>
+          ME({total.state === "loading" ? "loading..." : total.contents})
+        </button>
+      </>
     );
-  };
-
-  return (
-    <>
-      <button>HOME</button>
-      {renderButton("NETWORK", notifications.contents.network)}
-      {renderButton("MESSAGES", notifications.contents.messaging)}
-      {renderButton("JOBS", notifications.contents.jobs)}
-      {renderButton("NOTIFICATIONS", notifications.contents.notifications)}
-      <button>
-        ME({total.state === "loading" ? "loading..." : total.contents})
-      </button>
-    </>
-  );
+  } catch (e) {
+    console.error(`Error in Main App ${e}`);
+    return <div>Error occurred in Main App</div>;
+  }
 }
 
 function Todo({ id }: { id: number }) {
-  const todo = useRecoilValueLoadable(todoAtomFamily(id));
-  return todo.state === "loading" ? (
-    <>loading...</>
-  ) : (
-    <>
-      {todo?.contents.title}
-      {todo?.contents.description}
-      <br />
-    </>
-  );
+  try {
+    const todo = useRecoilValueLoadable(todoAtomFamily(id));
+    return todo.state === "loading" ? (
+      <>loading...</>
+    ) : (
+      <>
+        {todo?.contents.title}
+        {todo?.contents.description}
+        <br />
+      </>
+    );
+  } catch (e) {
+    console.error(`Error in Todo for ID ${id}: ${e}`);
+    return <div>Error occurred in Todo for ID {id}.</div>;
+  }
 }
 
 export default App;
