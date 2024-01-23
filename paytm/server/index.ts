46a0de31f80b79e7e98dac2c57467d16685ce0d9
arnnvv <arnav.900@protonmail.com>
2024-01-23 07:31:09 +0530

    router

--- paytm/server/index.ts
+++ paytm/server/index.ts
@@ -1,1 +1,9 @@
 import express from "express";
+import Users from "./db.ts";
+import router from "./routes/index.ts";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", router);
