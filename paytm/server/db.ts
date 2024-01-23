681c6298b6937bb4bb38f464ddaabd2ab58883d2
arnnvv <arnav.900@protonmail.com>
2024-01-23 07:26:55 +0530

    paytm

---
+++ paytm/server/db.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+mongoose.connect(
+  `mongodb+srv://arnav:arnavsharma1A@cluster0.n9borfi.mongodb.net/paytm`,
+);
+const Users = mongoose.model(`user`, {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+export default Users;
