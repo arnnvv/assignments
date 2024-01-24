be3731900facfc11082d3d3ae717f5d6d4898273
arnnvv <arnav.900@protonmail.com>
2024-01-24 16:52:32 +0530

    signIn

--- paytm/server/index.ts
+++ paytm/server/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import Users from "./db.ts";
 import router from "./routes/index.ts";
+import cors from "cors";
 
+const port: number = 3000;
 const app = express();
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/v1", router);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- paytm/server/routes/userRouter.ts
+++ paytm/server/routes/userRouter.ts
@@ -1,8 +1,84 @@
 import express, { Request, Response } from "express";
-
+import zod from "zod";
+import jwt from "jsonwebtoken";
+import Users from "../db";
+const JWT_SECRET: string = "UR_SECRET_KY";
 const userRouter = express.Router();
-userRouter.get("/", async (req: Request, res: Response) => {
-res.send("Hello from /users");
+
+interface User {
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
+
+const validate = (user: User) => {
+  const signupSchema = zod.object({
+    username: zod.string().min(3),
+    password: zod.string().min(6),
+    firstName: zod.string().min(3),
+    lastName: zod.string().min(3),
+  });
+  return signupSchema.safeParse(user);
+};
+
+userRouter.get("/", async (req: Request, res: Response) => {
+  res.send("Hello from /users");
+});
+
+userRouter.post("/signup", async (req: Request, res: Response) => {
+  const { user } = req.body;
+  if (!validate(user).success)
+    return res.status(401).json(`Invalid Credentials`);
+
+  try {
+    const existingUser = await Users.findOne({
+      useranme: user.username,
+    });
+
+    if (existingUser) return res.status(400).json("Username already exists");
+
+    await Users.create(user);
+    const userId = user._id;
+    const token: string | undefined = jwt.sign(userId, JWT_SECRET);
+    return res.status(200).json({
+      message: "User Created",
+      token: token,
+    });
+  } catch (error) {
+    console.error(`Error in creating user: ${error}`);
+    return res.status(500).json({
+      message: `Error in creating user: ${error}`,
+    });
+  }
+});
+
+userRouter.post("/signin", async (req: Request, res: Response) => {
+  const { user } = req.body;
+
+  if (!validate(user).success)
+    return res.status(401).json(`Invalid Credentials`);
+
+  try {
+    const existingUser = await Users.findOne({
+      username: user.username,
+    });
+
+    if (!existingUser) return res.status(401).json(`Username dosen't exist`);
+
+    if (existingUser.password !== user.password)
+      return res.status(401).json(`Wrong Password`);
+
+    const token: string | undefined = jwt.sign(existingUser._id, JWT_SECRET);
+    return res.status(200).json({
+      token: token,
+    });
+  } catch (error) {
+    console.error(`Error in Logging in: ${error}`);
+    return res.status(500).json({
+      message: `Error in Logging in`,
+    });
+  }
+});
 
 export default userRouter;
